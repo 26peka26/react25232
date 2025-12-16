14d1ee564d83fe8b5801b5702c5f2ee1f012cccf
26peka26 <26peka26@gmail.com>
2025-12-15 23:04:35 -0300

    Refactor Footer component and remove test link
    Removed test link and cleaned up footer component.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,22 +1,13 @@
+import { Link } from "react-router-dom";*/
 export const Footer = () => {
     return (
         <footer>
-            {/* ... Contenido normal del footer ... */}
-            
-            {/* ENLACE DE PRUEBA: ELIMINAR DESPUÉS */}
             <div style={{ padding: '10px', textAlign: 'center', backgroundColor: '#eee' }}>
                 <Link to="/admin/login" style={{ color: 'blue', fontWeight: 'bold' }}>
                     [BOTÓN DE PRUEBA: ACCESO ADMIN]
                 </Link>
             </div>
+            <p>Pagina creada dentro del proyecto React25232</p>
         </footer>
     );
 };
-/*export const Footer = () => {
-  return (
-    <footer>
-      <p>Pagina creada dentro del proyecto React25232</p>
-    </footer>
-  );
-};
-import { Link } from "react-router-dom";*/
