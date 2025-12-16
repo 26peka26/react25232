f5cad5d71652ad64cce67549acecfe8b91ffa987
26peka26 <26peka26@gmail.com>
2025-12-15 21:20:57 -0300

    Add logo image to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import { Nav } from "../Nav/Nav";
+import logo from '../../assets/logo.png';
 
 export const Header = () => {
   return (
     <header>
-      <Link to={"/"}>COMIC STORE</Link>
-      <Nav />
+      <Link to={"/"}>
+          <img src={logo} alt="Logo de la tienda de cómics" className="header-logo" />
+      </Link>
+      <Nav />
     </header>
   );
 };
