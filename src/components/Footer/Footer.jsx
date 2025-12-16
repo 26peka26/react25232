8482ff38fac7dbd5b02ced19481efd1c251a81e9
26peka26 <26peka26@gmail.com>
2025-12-15 23:37:33 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,13 +2,7 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
     return (
         <footer>
-            <div style={{ padding: '10px', textAlign: 'center', backgroundColor: '#eee' }}>
-                <Link to="/admin/login" style={{ color: 'blue', fontWeight: 'bold' }}>
-                    [BOTÓN DE PRUEBA: ACCESO ADMIN]
-                </Link>
-            </div>
-            
-            <p>Pagina creada dentro del proyecto React 25232</p>
+           <p>Pagina creada dentro del proyecto React 25232</p>
         </footer>
     );
 };
